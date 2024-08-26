895eb43b1888ea403d62cbb2f126659c925074dc
Silvia Pasifica <silvia.inawati@binus.ac.id>
2024-08-26 18:08:56 +0700

    detail page

--- src/components/DetailPage.vue
+++ src/components/DetailPage.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="">
-    <p class="text-left container mx-auto">Judul - Kategori</p>
-    <p class="text-5xl text-opacity-25 font-bold">basic game with horror genre</p>
-    <img class="h-[400px] w-full" alt="profile-picture" src="../assets/logo.png" />
+    <p class="text-left container mx-auto">Mobile Game - Game Developer</p>
+    <p class="text-5xl text-opacity-25 font-bold py-16">Pico Terra: Simulation Game</p>
+    <div class="overflow-hidden h-[400px] w-full">
+      <img class="" alt="profile-picture" src="../assets/bannerPicoTerra.png" />
+    </div>
 
     <div class="brief flex py-12 container mx-auto">
       <div class="w-1/3">
         <p class="font-semibold">The Brief</p>
       </div>
-      <div class="w-2/3 text-left">
+      <div class="w-2/3 text-left text-lg">
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-          specimen book.
+          This project is part of the Apple Developer Academy 2023 program. This game has the theme of Climate Change which allows players to manage the ecosystem provided. This game can enable players to understand the causes and
+          consequences of climate change as well as how to prevent and handle it appropriately.
         </p>
       </div>
     </div>
-    <div class="image flex gap-8 py-12 container mx-autos">
+    <div class="image flex gap-8 py-12 container mx-auto">
       <img class="h-[400px] w-full rounded-lg" alt="profile-picture" src="../assets/odp.jpeg" />
       <img class="h-[400px] w-full rounded-xl" alt="profile-picture" src="../assets/odp.jpeg" />
     </div>
@@ -24,6 +26,19 @@
         <p class="font-semibold">The Process</p>
       </div>
       <div class="w-2/3 text-left">
+        <p class="text-2xl font-semibold pb-4">Define Role & Contribution</p>
+        <div>
+          <p class="font-semibold">Team</p>
+          <ul>
+            <li>Silvia Pasifica Tri Inawati (Game Developer) - Me</li>
+            <li>Sheren Emanuela (Game Developer)</li>
+            <li>Maria Berliana Yapputri (Game Developer)</li>
+            <li>Albert Silva (Game Developer)</li>
+            <li>Kanaya (Game Designer)</li>
+            <li>Eros Kuncoro (Game Designer)</li>
+            <li>William Chandra (Project Manager)</li>
+          </ul>
+        </div>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
           specimen book.
@@ -35,9 +50,12 @@
         <p class="font-semibold">The Result</p>
       </div>
       <div class="w-2/3 text-left">
+        <p class="text-2xl font-semibold pb-4">About The App</p>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-          specimen book.
+          Pico Terra is not just a city management game; it's an innovative platform that empowers users to craft their ideal sustainable and eco-friendly city. In this game, players harness the power to construct buildings that not only
+          support the lives of virtual citizens but also contribute to maintaining a delicate balance in the ecosystem. Our vision extends beyond mere entertainment; we aspire to create a gaming experience that fosters positive societal
+          impact. By addressing the pressing issue of climate change, Pico Terra aims to convey the importance of combating global warming in a captivating and enjoyable manner. It's not just about building; it's about the ongoing effort to
+          sustain and nurture a harmonious environment. Our mantra is clear: Our Life, Our Hope. Pico Terra invites players to become the agents of change they wish to see in the virtual world and beyond.
         </p>
       </div>
     </div>
@@ -55,3 +73,15 @@ export default {
   },
 };
 </script>
+
+<style>
+ul {
+  list-style-type: disc;
+  padding-left: 20px;
+  margin-left: 20px;
+}
+
+li {
+  list-style-type: disc;
+}
+</style>
